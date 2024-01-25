ff1533550faa11bc7f0c219a37ceb17516dea36f
Dhananjay Sapawat <cs1190345@iitd.ac.in>
2024-01-25 15:57:08 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import Bar from './Bar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ const [inputtext, setInputtext] = useState("");
+ const api = "";
+ const debouncer = (func, delay) => {
+  let timeoutId;
+  return function (...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
+ return <>
+    <Bar/>
+    <input text = {inputtext}         onChange={(e) => {
+          setInputtext(e.target.value);
+          debouncer(e.target.value);
+        }} />
+  </>
+};
+
 
 export default App;
---
+++ src/Bar.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect } from 'react';
+
+import './Bar.css';
+const Bar = () => {
+    return <ul className = "bar">
+        <li className = "home">Home</li>
+        <li className = "about">About</li>
+    </ul>
+}
+export default Bar;
